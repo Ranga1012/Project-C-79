52a87dd42008539605df45cbca631a3663540647
Ranga1012 <83859129+Ranga1012@users.noreply.github.com>
2021-08-23 16:21:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,28 @@
-name_of_the_student_array = [];
+menu_list_array = ["Chicken Tandoori Pizza", "Deluxe Veg Pizza", "Paneer Tikka Pizza", "Veg Extravaganza Pizza","Veg Magherita Pizza","Veg Supreme Pizza",""];
 
-function submit(){
-
-    var display_student_array = [];
-
-    for (var k = 1 ; k <= 4 ; k++){
-
-        var name_of_the_student = document.getElementById("name_of_the_student_" + k).value;
-        console.log(name_of_the_student);
-        name_of_the_student_array.push(name_of_the_student);
-    }
-
-    console.log(name_of_the_student_array);
-    
-    var length_of_the_Student_array = name_of_the_student_array.length;
-    console.log(length_of_the_Student_array);
-
-    for (var i = 0 ; i < length_of_the_Student_array ; i++){
-
-        display_student_array.push("<h4> NAME - " + name_of_the_student_array[i] + "</h4>");
-        console.log(display_student_array);
-    }
-
-    console.log(display_student_array);
-    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
-
-    var remove_commas = display_student_array.join(" ");
-    console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
-
-    document.getElementById("submit_button").style.display = "none";
-    document.getElementById("sort_button").style.display = "inline-block";
-
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i = 0 ; i < menu_list_array.length ; i++){
+            htmldata = htmldata + menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
 }
 
-function sorting() {
-    name_of_the_student_array.sort();
-    console.log(name_of_the_student_array);
-    
-    var display_student_array_sorting = [];
-    var length_of_name_of_student_array = name_of_the_student_array.length;
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        menu_list_array.sort();
+        htmldata = menu_list_array;
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata = htmldata + imgtags + menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
 
-    console.log(length_of_name_of_student_array);
-
-    for (var j = 0 ; j < length_of_name_of_student_array ; j++){
-
-        display_student_array_sorting.push("<h4> NAME -" + name_of_the_student_array[j] + "</h4>");
-        console.log(display_student_array_sorting);
-    }
-    var remove_commas = display_student_array_sorting.join(" ");
-    console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
-}
-
-function new_update(){
-document.getElementById("display_name_without_commas").innerHTML = "<h1>" + name_of_the_student_array + "</h1>";
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
